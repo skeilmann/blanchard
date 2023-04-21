1897e89425720d1b7ffd0f6707ae04c88cf73e5c
@gheorghe_cazacu <gheorghe91@hotmail.com>
2023-04-21 23:11:48 +0300

    still working on search button

--- js/search.js
+++ js/search.js
@@ -1,49 +1,51 @@
-(() => {
-  function setSearch(params) {
-    const openBtn = document.querySelector(`.${params.openBtnClass}`);
-    const search = document.querySelector(`.${params.searchClass}`);
-    const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
+function setSearch(params) {
+  const openBtn = document.querySelector(`.${params.openBtnClass}`);
+  const search = document.querySelector(`.${params.searchClass}`);
+  const closeBtn = search.querySelector(`.${params.closeBtnClass}`);
 
-    search.addEventListener("animationend", function (evt) {
-      if (this._isOpened) {
-        this.classList.remove(params.activeClass);
-        this.classList.remove(params.hiddenClass);
-        this._isOpened = false;
-      } else {
-        this._isOpened = true;
-      }
-    });
+  search.addEventListener("animationend", function (evt) {
+    if (this._isOpened) {
+      this.classList.remove(params.activeClass);
+      this.classList.remove(params.hiddenClass);
+      this._isOpened = false;
+    } else {
+      this._isOpened = true;
+    }
+  });
 
-    search.addEventListener("click", function (evt) {
-      evt._isSearch = true;
-    });
+  search.addEventListener("click", function (evt) {
+    evt._isSearch = true;
+  });
 
-    openBtn.addEventListener("click", function (evt) {
+  openBtn.addEventListener("click", function (evt) {
+    this.disabled = true;
 
-      if (
-        !search.classList.contains(params.activeClass) &&
-        !search.classList.contains(params.hiddenClass)
-      ) {
-        search.classList.add(params.activeClass);
-      }
-    });
+    if (
+      !search.classList.contains(params.activeClass) &&
+      !search.classList.contains(params.hiddenClass)
+    ) {
+      search.classList.add(params.activeClass);
+    }
+  });
 
-    closeBtn.addEventListener("click", function () {
+  closeBtn.addEventListener("click", function () {
+    openBtn.disabled = false;
+    search.classList.add(params.hiddenClass);
+  });
+
+  document.body.addEventListener("click", function (evt) {
+    if (!evt._isSearch && search._isOpened) {
+      openBtn.disabled = false;
       search.classList.add(params.hiddenClass);
-    });
-
-    document.body.addEventListener("click", function (evt) {
-      if (!evt._isSearch && search._isOpened) {
-        search.classList.add(params.hiddenClass);
-      }
-    });
-  }
+    }
+  });
 
   setSearch({
-    searchClass: "js-form", // класс формы поиска
     openBtnClass: "js-open-search", // класс кнопки открытия
     closeBtnClass: "js-close", // класс кнопки закрытия
+    searchClass: "js-form", // класс формы поиска
     activeClass: "is-opened", // класс открытого состояния
     hiddenClass: "is-closed", // класс закрывающегося состояния (удаляется сразу после закрытия)
   });
-})();
+}
+//'""
